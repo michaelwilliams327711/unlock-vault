273489a2fffff180d49f97afb73957bfb6c18200
michael williams <saintwill327711@gmail.com>
2023-09-15 19:22:49 -0400

    display pin to user

--- main.js
+++ main.js
@@ -1,1 +1,10 @@
-alert("its working");
+//create variable to store the user prompt.
+let userMessage =
+  "You have received this message because you have been chosen to open an important vault. Here is the secret combination:";
+// calculate first pin number and store to a variable
+let pin1 = 5 + 5;
+// calculate second pin number and store to a variable
+let pin2 = 100 - 60;
+// calculate third pin number and store to a variable
+let pin3 = 3 * 13;
+alert(userMessage + " " + pin1 + " " + pin2 + " " + pin3);
